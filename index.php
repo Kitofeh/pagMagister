786b94144450af21dc48ac7f75593b0c2e55fbcd
Nicolas Contreras <nmj.contreras@gmail.com>
2023-11-18 17:45:30 -0300

    Creacion de la plantilla principal junto con fichero .css

--- index.php
+++ index.php
@@ -1,17 +1,88 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Web Magister UDA</title>
 </head>
 <body>
-    <?php
+    <header>
+        <section><!--Barra de navegacion-->
+            <nav>
+                <ul>
+                    <li><a href="#DescripcionGeneral">Descripcion General</a></li>
+                    <li><a href="#NoticiasYEventos">Noticias y Eventos</a></li>
+                    <li><a href="#AcercaDelPosgrado">Acerca del Posgrado </a></li>
+                    <li><a href="#Admision">Admision</a></li>
+                    <li><a href="#Requisitos">Requisitos</a></li>
+                    <li><a href="#PlanDeEstudios">Plan de estudios</a></li>
+                    <li><a href="#Experiencias">Experiencias</a></li>
+                    <li><a href="#Recursos">Recursos</a></li>
+                    <li><a href="#Contacto">Contacto</a></li>
+                    <li><a href="#PreguntasFrecuentes">Preguntas Frecuentes</a></li>
+                </ul>
+            </nav>
+        </section>
 
-        echo "Hola, mundo"
+        <section><!--Banner-->
 
-    ?>
+        </section>
+
+    </header>
+
+    <main><!--Cuerpo de la Web-->
+
+        <section id="DescripcionGeneral" class="section-margin"><!--Descripcion General-->
+            
+        </section>
+
+        <section id="NoticiasYEventos" class="section-margin"><!--Noticias y Eventos-->
+        
+        </section>
+
+        <section id="AcercaDelPosgrado" class="section-margin"><!--Acerca del Posgrado-->
+            
+        </section>
+
+        <section id="Admision" class="section-margin"><!--Admision-->
+            
+        </section>
+
+        <section id="Requisitos" class="section-margin"><!--Requisitos-->
+            
+        </section>
+
+        <section id="PlanDeEstudios" class="section-margin"><!--Plan de Estudios-->
+            
+        </section>
+
+        <section id="Experiencias" class="section-margin"><!--Experiencias-->
+            
+        </section>
+
+        <section id="Recursos" class="section-margin"><!--Recursos-->
+            
+        </section>
+
+        <section id="Contactos" class="section-margin"><!--Contacto-->
+            
+        </section>
+
+        <section id="PreguntasFrecuentes" class="section-margin"><!--Preguntas mas frecuentes-->
+            
+        </section>
+
+    </main>
+
+    <section class="section-margin"><!--Footer-->
+        <footer>
+
+        </footer>
+    </section>
+
 </body>
 </html>
+
+
 
