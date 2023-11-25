228c5be2ffffce9ef99d5278254f5ab3b52bd5ef
Nicolas Contreras <nmj.contreras@gmail.com>
2023-11-24 21:25:25 -0300

    Mejora de la NavBar, Creacion Seccion Acerca de Nosotros, Se agregaron nuevos elementos (imagenes y logos institucionales)

--- index.php
+++ index.php
@@ -3,38 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Web Magister UDA</title>
 </head>
-<body>
+<body style="font-family: 'Montserrat', sans-serif;">
     <header>
         <section><!--Barra de navegacion-->
-            <nav>
-                <ul>
-                    <li><a href="#DescripcionGeneral">Descripcion General</a></li>
-                    <li><a href="#NoticiasYEventos">Noticias y Eventos</a></li>
-                    <li><a href="#AcercaDelPosgrado">Acerca del Posgrado </a></li>
-                    <li><a href="#Admision">Admision</a></li>
-                    <li><a href="#Requisitos">Requisitos</a></li>
-                    <li><a href="#PlanDeEstudios">Plan de estudios</a></li>
-                    <li><a href="#Experiencias">Experiencias</a></li>
-                    <li><a href="#Recursos">Recursos</a></li>
-                    <li><a href="#Contacto">Contacto</a></li>
-                    <li><a href="#PreguntasFrecuentes">Preguntas Frecuentes</a></li>
+            <nav class="py-4 fixed-top" style="background-color: rgba(255, 255, 255, 0.1);backdrop-filter: blur(10px); box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">
+                <ul style="margin: 0px 20px;">
+                    <li class="pr-2"><a href="#Nosotros" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Nosotros</a></li>
+                    <li class="px-2"><a href="#NoticiasYEventos" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Noticias y Eventos</a></li>
+                    <li class="px-2"><a href="#AcercaDelPosgrado" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Acerca del Posgrado </a></li>
+                    <li class="px-2"><a href="#Admision" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Admision</a></li>
+                    <li class="px-2"><a href="#Requisitos" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Requisitos</a></li>
+                    <li class="px-2"><a href="#PlanDeEstudios" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Plan de estudios</a></li>
+                    <li class="px-2"><a href="#Experiencias" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Experiencias</a></li>
+                    <li class="px-2"><a href="#Recursos" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Recursos</a></li>
+                    <li class="px-2"><a href="#Contacto" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Contacto</a></li>
+                    <li class="px-2"><a href="#PreguntasFrecuentes" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 1);">Preguntas Frecuentes</a></li>
                 </ul>
             </nav>
         </section>
 
-        <section><!--Banner-->
-
+        <section class="section-margin" style="margin: 0px 0px;"><!--Banner-->
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img-banner/img-dicc-1.jpg" height="700px;" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img-banner/img-dicc-2.jpg" height="700px;" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img-banner/img-dicc-1.jpg" height="700px;" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
         </section>
 
     </header>
 
     <main><!--Cuerpo de la Web-->
 
-        <section id="DescripcionGeneral" class="section-margin"><!--Descripcion General-->
-            
+        <section id="Nosotros" class="section-margin" style="margin: 100px 0px; display: flex; flex-direction: column; width: 100%; height: 750px;"><!--Descripcion General-->
+            <div style="display: flex; justify-content: center; align-items: center; background-color: white; width: 100%; height: 150px;">
+                <h2 style="box-shadow: inset 0 -2px 0 #dbca11">Acerca de Nosotros</h2>
+            </div>
+            <div style="background-color: #dbca11; width: 100%; height: 300px;">
+                <img src="img-about-us.jpg" style="width: 100%; height: 100%; object-fit: cover;">
+            </div>
+            <div style="display: flex; justify-content: center; align-items: center; background-color: #364c59; width: 100%; height: 300px;">
+                <p style="color: white; margin: 0px 250px;">El Departamento de Ingeniería Informática y Ciencias de la Computación ha estado ofreciendo el Magíster de Investigación desde 2013 con el objetivo de fomentar el desarrollo de habilidades avanzadas en investigación y potenciar la generación de conocimientos científicos en áreas vinculadas a las tecnologías de la información y las ciencias de la computación.</p>
+            </div>
         </section>
 
         <section id="NoticiasYEventos" class="section-margin"><!--Noticias y Eventos-->
